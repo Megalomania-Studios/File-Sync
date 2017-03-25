b1bdec632887d4bb733e643b7a348413c470df6d
Olfi01 <flom@meyer-alpers.de>
2017-03-25 13:17:32 +0100

    Added custom classes SyncOrder and SyncRules

---
+++ Megalomania Studios Filesync/CustomSubclasses/SyncOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megalomania_Studios_Filesync.CustomSubclasses
+{
+    public class SyncOrder
+    {
+        public string pathFrom { get; set; }
+        public string pathTo { get; set; }
+        public SyncRules syncSettings { get; set; }
+        public SyncOrder(string from, string to, SyncRules settings)
+        {
+            pathFrom = from;
+            pathTo = to;
+        }
+    }
+}
---
+++ Megalomania Studios Filesync/CustomSubclasses/SyncRules.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megalomania_Studios_Filesync.CustomSubclasses
+{
+    public class SyncRules
+    {
+        /*add any order-specific settings here as fields,
+        add a static default value for them and use them in constructor*/
+        public SyncRules()
+        {
+            /*for every setting, assign the given value to their field*/
+        }
+    }
+}
