83c323d697adcd2c5e2ad1141e1dd91fa688a961
Olfi01 <flom@meyer-alpers.de>
2017-03-25 13:22:33 +0100

    Added default for SyncRules

--- Megalomania Studios Filesync/CustomSubclasses/SyncOrder.cs
+++ Megalomania Studios Filesync/CustomSubclasses/SyncOrder.cs
@@ -11,8 +11,12 @@ namespace Megalomania_Studios_Filesync.CustomSubclasses
         public string pathFrom { get; set; }
         public string pathTo { get; set; }
         public SyncRules syncSettings { get; set; }
-        public SyncOrder(string from, string to, SyncRules settings)
+        public SyncOrder(string from, string to, SyncRules settings = null)
         {
+            if (settings == null)
+            {
+                settings = SyncRules.Default;
+            }
             pathFrom = from;
             pathTo = to;
         }
--- Megalomania Studios Filesync/CustomSubclasses/SyncRules.cs
+++ Megalomania Studios Filesync/CustomSubclasses/SyncRules.cs
@@ -10,6 +10,15 @@ namespace Megalomania_Studios_Filesync.CustomSubclasses
     {
         /*add any order-specific settings here as fields,
         add a static default value for them and use them in constructor*/
+
+        public static SyncRules Default
+        {
+            get
+            {
+                return new SyncRules();
+            }
+        }
+        
         public SyncRules()
         {
             /*for every setting, assign the given value to their field*/
