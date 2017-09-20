f93eb81bb5ff8f6637e8214260e1e7955f669899
Olfi01 <flom@meyer-alpers.de>
2017-09-20 21:54:54 +0200

    Vielleicht tut es jetzt schon... gucke gleich.

--- MSFileSyncer/Classes/SyncRules.cs
+++ MSFileSyncer/Classes/SyncRules.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace MSFileSyncer.Classes
 {
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     public class SyncRules
     {
         /*add any order-specific settings here as fields,
--- MSFileSyncer/Program.cs
+++ MSFileSyncer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MSFileSyncer.Classes;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,18 +11,42 @@ namespace MSFileSyncer
 {
     class Program
     {
-        private const string relativeSyncFilePath = ".msfilesync\\order.sync";
+        private const string relativeSyncFilePath = ".mvsfilesync\\order.sync";
 
         static void Main(string[] args)
         {
-            //Laufwerksbuchstabe sollte als erstes Argument übergeben werden
+            //Drive letter should be first argument
             if (args.Length < 1) throw new ArgumentOutOfRangeException("drive", "No drive letter supplied.");
             var driveLetter = args[0];
 
+            //Get sync info file and deserialize it
             var syncFilePath = Path.Combine(driveLetter, relativeSyncFilePath);
             if (!File.Exists(syncFilePath)) return;
             var syncFileContent = File.ReadAllText(syncFilePath);
+            var syncOrder = JsonConvert.DeserializeObject<SyncOrder>(syncFileContent);
+
+            DeepCopy(syncOrder.OriginFolder, syncOrder.DestinationFolder);
+        }
+
+        private static void DeepCopy(string originFolder, string destinationFolder)
+        {
+            var from = new DirectoryInfo(originFolder);
+            var to = new DirectoryInfo(destinationFolder);
+            //check for target directory being in origin directory to prevent copying over and over
+            if (to.FullName.Contains(from.FullName)) throw new Exception("Folder nesting detected. Cancelling deepcopy.");
             
+            //call this method recursively for all subdirectories
+            foreach (var originDir in from.EnumerateDirectories())
+            {
+                var subdir = to.CreateSubdirectory(originDir.Name);
+                DeepCopy(originDir.FullName, subdir.FullName);
+            }
+
+            //copy all files in this directory
+            foreach (var fileInfo in from.EnumerateFiles())
+            {
+                fileInfo.CopyTo(Path.Combine(to.FullName, fileInfo.Name));
+            }
         }
     }
 }
