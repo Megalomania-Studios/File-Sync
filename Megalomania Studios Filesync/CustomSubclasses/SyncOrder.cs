12c848c5ee838c3d38412d8b42b41b5ab0754ca6
Olfi01 <flom@meyer-alpers.de>
2017-09-20 12:38:02 +0200

    Corrected Properties due to naming convention

--- Megalomania Studios Filesync/CustomSubclasses/SyncOrder.cs
+++ Megalomania Studios Filesync/CustomSubclasses/SyncOrder.cs
@@ -8,17 +8,17 @@ namespace Megalomania_Studios_Filesync.CustomSubclasses
 {
     public class SyncOrder
     {
-        public string pathFrom { get; set; }
-        public string pathTo { get; set; }
-        public SyncRules syncSettings { get; set; }
+        public string PathFrom { get; set; }
+        public string PathTo { get; set; }
+        public SyncRules SyncSettings { get; set; }
         public SyncOrder(string from, string to, SyncRules settings = null)
         {
             if (settings == null)
             {
                 settings = SyncRules.Default;
             }
-            pathFrom = from;
-            pathTo = to;
+            PathFrom = from;
+            PathTo = to;
         }
     }
 }
