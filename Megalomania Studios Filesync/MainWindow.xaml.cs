c6738ea06c05ea23bcc3e6e32f9cacf6ace1d762
Jupp56 <kennichnicht100@gmail.com>
2017-09-21 14:04:44 +0200

    GUI+sonstiges

--- Megalomania Studios Filesync/MainWindow.xaml.cs
+++ Megalomania Studios Filesync/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using Microsoft.Win32;
+using windowsforms = System.Windows.Forms;
 
 
 
@@ -30,12 +32,43 @@ namespace Megalomania_Studios_Filesync
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    
+
+    #region Main class
     public partial class MainWindow : Window
     {
+
+        #region Constructor
+
+        public MainWindow()
+        {
+
+
+            InitializeComponent();
+            BackupIsActivated = false;
+            //Status des Backups und Geräteliste laden und Darstellen
+            ReloadState();
+            //this.DataContext = fold;
+
+
+            //if (!Settings.Default.HasBeenInstalled)
+            {
+                //install();
+            }
+            //ServiceController service = new ServiceController("SyncService");
+            //service.Start();
+
+        }
+
+        #endregion
+
+        #region Update methods
+
+            #region ReloadState (Update)
+
         //läd den Status des Backups neu, zeigt aktiv oder inaktiv und den jeweils richtigen Button an. Läd außerdem die Geräteliste neu und SOLL auch die Ordnerliste neu laden.
         public void ReloadState()
         {
+            Folders.ItemsSource = Foldersource;
             Deviceact();
             Folderact();
             Backact();
@@ -53,6 +86,9 @@ namespace Megalomania_Studios_Filesync
             }
 
         }
+        #endregion
+
+            #region Backact (is Backup activated?)
         public void Backact()
         {
             //hier statt Wertabfrage der schon deklarierten variablen (zu testzwecken) Abfragemechanismus einbauen
@@ -74,10 +110,13 @@ namespace Megalomania_Studios_Filesync
             else
                 //falls irgendwas schiefläuft
                 BackupIsActivated = false;
-            //hier und auch sonst später den (noch kommenden) Errorcodehandler (Methode, der man den Errorcode zum Fraß vorwirft) konsultieren
+            //hier und auch sonst später den (noch kommenden) Errorcodehandler (Methode, der man den Errorcode zum Fraß vorwirft ;-)) konsultieren
             MessageBox.Show("Ein Fehler ist aufgetreten. (Code: 0x00002) Der Status des Backups konnte nicht erkannt werden.", "Fehler bei der Erkennung des Backupzustandes");
             return;
         }
+        #endregion
+
+            #region Deviceact (Update of connected devices)
 
         public void Deviceact()
         {
@@ -102,68 +141,80 @@ namespace Megalomania_Studios_Filesync
                     MessageBox.Show(ex.ToString());
                 }
 
-
-
             }
 
-            //items.Add(new Devices() { DLetter = "F://", Name = " "+ "USB"});
-
-            //items.Add(new Devices() { DLetter = "Usbgerät (G://)" });
             Devices.ItemsSource = items;
 
+            Folderact();
+
         }
+        #endregion
+
+            #region  Folderact (Update of Folders based on file?)
 
         public void Folderact()
         {
 
 
-            /*List<Folders> items = new List<Folders>();
-            Folders.ItemsSource = items;
-            items.Add(new Folders() { OriginFolder = "Origin", DestinationFolder = "Destiny", SyncTime = "SyncTime" });
-            items.Add(new Folders() { OriginFolder = "Origin2", DestinationFolder = "Destiny3", SyncTime = "SyncTime2" });*/
-
-        }
-        //was das hier werden soll weiß ich noch nicht
-        /*public void Folderact()
-        {
-            ObservableCollection<string> Folder = new ObservableCollection<string>();
-        }*/
-        //public ObservableCollection<string> Folder { get; private set; }
-        //public ICollection<string> Ordner { get; private set; }
-
-
-        public MainWindow()
-        {
-
-
-            InitializeComponent();
-            BackupIsActivated = false;
-            //Status des Backups und Geräteliste laden und Darstellen
-            ReloadState();
-            //this.DataContext = fold;
-
-
-
-
-
-
-            //if (!Settings.Default.HasBeenInstalled)
+            if (((Devices)Devices.SelectedItem) == null)
             {
-                //install();
+                Foldersource.Clear();
+                Foldersource.Add(new Folders() { OriginFolder = "Kein Gerät ausgewählt" });
+                Folders.IsEnabled = false;
+                return;
             }
-            //ServiceController service = new ServiceController("SyncService");
-            //service.Start();
+            else
+            {
+                
+                string Devicescuritem = ((Devices)Devices.SelectedItem).Name;
+                //string Laufwerksbuchstabe = Devices.FindName(Devicescuritem).ToString();
+                MessageBox.Show(Devicescuritem);
+                MessageBox.Show(File.Exists(Path.Combine(Devicescuritem, "test.txt")).ToString());
+                if (!File.Exists(Path.Combine(Devicescuritem, "test.txt")))
+                {
+                    Folders.IsEnabled = false;
+                    MessageBox.Show("Errungenschaft freigeschaltet: Die M0n0t0nie des Lebens! Keine Datei zum auslesen gefunden!");
+                    Foldersource.Clear();
 
+                    Foldersource.Add(new Folders() { OriginFolder = "Keine Ordner gefunden" });
+
+                    
+                    return;
+                }
+                else
+                {
+
+                    Folders.IsEnabled = true;
+                    Foldersource.Clear();
+                    string folders = File.ReadAllText(Path.Combine(Devicescuritem + "test.txt"));
+                    var list = JsonConvert.DeserializeObject<List<Folders>>(folders);
+
+                    foreach (var f in list)
+                    {
+                        Foldersource.Add(f);
+                    }                  
+
+                }
+            }
         }
 
-        //Folders fold = new Folders { OriginFolder = "Mannfred", DestinationFolder = "5", SyncTime = "Köln" };
+        #endregion
 
+            #region installer (work in Progress)
 
         private void install()
         {
             //throw new NotImplementedException();
         }
 
+        #endregion
+
+        #endregion
+
+        #region Clickhandlers
+
+        #region selectionchanged methods
+        #region Backupstate Button ((de)activation)
         private void BackupstateChange_Click(object sender, RoutedEventArgs e)
         {
 
@@ -187,56 +238,15 @@ namespace Megalomania_Studios_Filesync
                 return;
             }
         }
+        #endregion
 
-        public bool BackupIsActivated { get; set; }
 
-
-        private void ListBoxItem_Selected(object sender, EventArgs e)
-        {
-            //hier muss dann die Datenbankabfrage für das entsprechende Gerät implementiert werden
-        }
 
         private void Devices_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
-            string Devicescuritem = ((Devices)Devices.SelectedItem).Name;
-            //string Laufwerksbuchstabe = Devices.FindName(Devicescuritem).ToString();
-            MessageBox.Show(Devicescuritem);
-            MessageBox.Show(File.Exists(Path.Combine(Devicescuritem, "test.xml")).ToString());
-            if (!File.Exists(Path.Combine(Devicescuritem, "test.xml")))
-            {
-                MessageBox.Show("Errungenschaft freigeschaltet: Die M0n0t0nie des Lebens! Keine Datei zum auslesen gefunden!");
-                return;
-            }
-            else
-            {
-
-                
-                string folders = File.ReadAllText(@"D:\test.txt");
-                JsonConvert.DeserializeObject<List<Folders>>(folders);
-                List<Folders> items = new List<Folders>();
-                Folders.ItemsSource = items;
-
-
-
-
-
-
-                /*List<Folders> objectstoserialize = new List<Folders>();
-                FileStream fs = new FileStream(@"D:\test.xml", FileMode.Open); ;
-                BinaryFormatter formatter = new BinaryFormatter();
-                try
-                {
-                    objectstoserialise = (List<Folders>)formatter.Deserialize(fs);
-                    Folders.ItemsSource = objectstoserialise;
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Keine Ordnerpaare gefunden");
-                }*/
-
-
-            }
+            Folderact();
+            
         }
 
         private void Folders_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -244,62 +254,93 @@ namespace Megalomania_Studios_Filesync
 
         }
 
+        #endregion
+
+        #region button_clicks
+
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            var folders = Folders.ItemsSource;
-            var serialized = JsonConvert.SerializeObject(folders);
-            File.WriteAllText(@"D:\test.txt", serialized);
-
-
-            /* FileStream stream;
-             stream = new FileStream(@"D:\\test.xml", FileMode.Create);
-             BinaryFormatter formatter = new BinaryFormatter();
-             formatter.Serialize(stream, objectstoserialise);
-             stream.Close();*/
-
-
-
-
-
-
-
-
-
-            //Serialisation.SerializeObject(objectstoserialise, "test.xml");
+            if (((Devices)Devices.SelectedItem) == null)
+            {
+                MessageBox.Show("Kein Gerät ausgewählt! Bitte wählen sie zunächst ein Gerät", "Fehler");
+                return;
+            }
+            else
+            {
+                string Devicescuritem = ((Devices)Devices.SelectedItem).Name;
+                MessageBox.Show(Devicescuritem);
+                var folders = Folders.ItemsSource;
+                var serialized = JsonConvert.SerializeObject(folders);
+                File.WriteAllText(Path.Combine(Devicescuritem + "test.txt"), serialized);
+            }
 
         }
 
+        private void Reload_Click(object sender, RoutedEventArgs e)
+        {
+            Deviceact();
+        }
 
+        private void AddNewPair_Click(object sender, RoutedEventArgs e)
+        {
+            if (((Devices)Devices.SelectedItem) == null)
+            {
+                MessageBox.Show("Kein Gerät ausgewählt! Bitte wählen sie zunächst ein Gerät", "Fehler");
+                return;
+            }
+            else
+            {
+                
+                var folderdialog = new windowsforms.FolderBrowserDialog();
+                folderdialog.Description = "Ursprungsordner auswählen";
+                folderdialog.ShowNewFolderButton = false;
+                folderdialog.ShowDialog();
+                string originpath = folderdialog.SelectedPath;
+                folderdialog.Reset();
+                folderdialog.ShowNewFolderButton = false;
+                folderdialog.Description= "Zielordner auf dem Backup-Gerät auswählen";
+                folderdialog.RootFolder = Environment.SpecialFolder.MyComputer;
+                folderdialog.ShowDialog();
+                string destinationpath = folderdialog.SelectedPath;               
+                Foldersource.Add(new Folders { OriginFolder = originpath, DestinationFolder = destinationpath, SyncTime = "immer" });
 
-
-
+            }
+       
 
     }
-
-    /*public static class Serialisation
-    {
-        public static void SerializeObject(this List<string> list, string fileName)
+        private void DeletePair_Click(object sender, RoutedEventArgs e)
         {
-            var serializer = new XmlSerializer(typeof(List<String>));
-            using (var stream = File.OpenWrite(fileName))
+            if (((Devices)Devices.SelectedItem) == null)
             {
-                serializer.Serialize(stream, list);
+                MessageBox.Show("Kein Gerät ausgewählt! Bitte wählen sie zunächst ein Gerät", "Fehler");
+                return;
+            }
+            else
+            {
+                Foldersource.Remove(((Folders)Folders.SelectedItem));
             }
         }
-        public static void DeserializeObject(this List<string> list, string fileName)
-        {
-            var serializer = new XmlSerializer(typeof(List<String>));
-            using (var stream = File.OpenRead(fileName))
-            {
-                var other = (List<string>)(serializer.Deserialize(stream));
-                list.Clear();
-                list.AddRange(other);
-            }
-        }
-    }*/
 
-    //wichtig für die Geräteliste
-    #region variablesinclasses
+        #endregion
+
+        #endregion
+
+        #region class-internal variables
+
+        private ObservableCollection<Folders> Foldersource = new ObservableCollection<Folders>();
+
+        public bool BackupIsActivated { get; set; }
+
+
+
+
+        #endregion
+
+      
+    }
+    #endregion
+
+    #region variables in independent classes
 
     public class Devices
     {
