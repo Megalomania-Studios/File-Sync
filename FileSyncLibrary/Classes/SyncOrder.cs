acb77a7b5d0a0933bac099be8374a7018459a9fc
Olfi01 <flom@meyer-alpers.de>
2017-09-21 21:41:31 +0200

    added filesync library

--- FileSyncLibrary/Classes/SyncOrder.cs
+++ FileSyncLibrary/Classes/SyncOrder.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MSFileSyncer.Classes
+namespace FileSyncLibrary
 {
     [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     public class SyncOrder
