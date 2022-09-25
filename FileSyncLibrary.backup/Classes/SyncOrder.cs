7708ed98cbfd9866681a5e38e507724a00319935
Jupp56 <nick.gruessner@outlook.de>
2022-09-25 18:44:21 +0200

    Upgrade to .Net 6.0

---
+++ FileSyncLibrary.backup/Classes/SyncOrder.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSyncLibrary
+{
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class SyncOrder
+    {
+        //folder to copy from
+        [JsonProperty(PropertyName = "origin_folder", Required = Required.Always)]
+        public string OriginFolder { get; set; }
+        //folder to copy to
+        [JsonProperty(PropertyName = "destination_folder", Required = Required.Always)]
+        public string DestinationFolder { get; set; }
+        //settings for this sync order
+        [JsonProperty(PropertyName = "sync_settings", Required = Required.Always)]
+        public SyncRules Settings { get; set; }
+        //DateTime when this was last synced
+        [JsonProperty(PropertyName = "last_synced", Required = Required.Default)]
+        public DateTime LastSynced { get; set; }
+
+        public SyncOrder(string from, string to, SyncRules settings = null)
+        {
+            if (settings == null)
+            {
+                settings = SyncRules.Default;
+            }
+            Settings = settings;
+            OriginFolder = from;
+            DestinationFolder = to;
+        }
+    }
+}
---
+++ FileSyncService.backup/ProjectInstaller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileSyncService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+#pragma warning disable IDE1006 // Benennungsstile
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+#pragma warning restore IDE1006 // Benennungsstile
+        {
+            SetInterActWithDeskTop();
+        }
+
+        private static void SetInterActWithDeskTop()
+        {
+            var service = new System.Management.ManagementObject(
+                    String.Format("WIN32_Service.Name='{0}'", "MegalomaniaStudiosFileSyncService"));
+            try
+            {
+                var paramList = new object[11];
+                paramList[5] = true;
+                service.InvokeMethod("Change", paramList);
+            }
+            finally
+            {
+                service.Dispose();
+            }
+        }
+
+    }
+}
